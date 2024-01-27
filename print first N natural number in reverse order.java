c3e36ee527c6320798085c6e0b3294afcbd04840
vaishnavi <157712009+Vaishnavi-rr@users.noreply.github.com>
2024-01-27 15:47:42 +0530

    Create print first N natural number in reverse order.java

---
+++ print first N natural number in reverse order.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+public static void main (String arg[]){
+Scanner sc = new Scanner(System.in);
+int N = sc.nextInt();
+for(int i=N;i>0;i--){
+System.out.print(i+" ");
+}
+}
+}
